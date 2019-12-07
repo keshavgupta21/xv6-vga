4153ccc32dc81e8dd6f8f29d3daa50bab89ea354
Keshav Gupta <keshav21@mit.edu>
2019-12-06 22:59:21 -0500

    Update Mandelbrot set generator

--- user/brot.c
+++ user/brot.c
@@ -10,16 +10,19 @@
 #define WINDOW_HEIGHT 95
 #define PADDING_SIZE 1
 
+#define UP_ARROW 'w'
+#define DOWN_ARROW 's'
+#define RIGHT_ARROW 'd'
+#define LEFT_ARROW 'a'
+#define ZOOM_IN 'q'
+#define ZOOM_OUT 'e'
+
+#define RES (1 << 28)
+
 char fbuf[WINDOW_HEIGHT][WINDOW_WIDTH];
 
-static void draw_background() {
-  for (int i = 0; i < WINDOW_HEIGHT; i++) {
-    for (int j = 0; j < WINDOW_WIDTH; j++) {
-      fbuf[i][j] = BACKGROUND_COLOR;
-    }
-  }
-  return;
-}
+long long xmax = (1*RES)/2, ymax = (5*RES)/4, xmin = -2*RES, ymin = -(5*RES)/4;
+volatile int update_window = 1;
 
 static void draw_padding() {
   for (int i = 0; i < WINDOW_HEIGHT; i++) {
@@ -33,9 +36,7 @@ static void draw_padding() {
   return;
 }
 
-#define RES (1 << 28)
-
-void draw_brot(long long xmin, long long xmax, long long ymin, long long ymax) {
+void draw_brot() {
   for (int y = 0; y < WINDOW_HEIGHT; y++) {
     for (int x = 0; x < WINDOW_WIDTH; x++) {
       long long cr = (((long long)x) * (xmax - xmin))/WINDOW_WIDTH + xmin;
@@ -56,13 +57,51 @@ void draw_brot(long long xmin, long long xmax, long long ymin, long long ymax) {
   }
 }
 
-void main(void) {
-  draw_background();
-  long long xmax = (1*RES)/2, ymax = (5*RES)/4, xmin = -2*RES, ymin = -(5*RES)/4;
-  draw_brot(xmin, xmax, ymin, ymax);
+void key_handle(uint64 key0, uint64 key1) {
+  long long dx = (xmax - xmin)/10;
+  long long dy = (ymax - ymin)/10;
+  if (key1 == UP_ARROW) {
+    ymin -= dy;
+    ymax -= dy;
+  } else if (key1 == DOWN_ARROW) {
+    ymin += dy;
+    ymax += dy;
+  } else if (key1 == LEFT_ARROW) {
+    xmin -= dx;
+    xmax -= dx;
+  } else if (key1 == RIGHT_ARROW) {
+    xmin += dx;
+    xmax += dx;
+  } else if (key1 == ZOOM_IN) {
+    xmin += dx;
+    xmax -= dx;
+    ymin += dy;
+    ymax -= dy;
+  } else if (key1 == ZOOM_OUT) {
+    xmin -= dx;
+    xmax += dx;
+    ymin -= dy;
+    ymax += dy;
+  }
+  // printf("xmin = %d, xmax = %d, ymin = %d, ymax = %d\n", xmin, xmax, ymin, ymax);
+  draw_brot();
   draw_padding();
-  show_window((char*) fbuf);
-  while(1);
+  update_window = 1;
+  cb_return();
+}
+
+void main(void) {
+  reg_keycb(key_handle);
+  draw_brot();
+  draw_padding();
+  while (1) {
+    // printf("here\n");
+    if (update_window) {
+      printf("updating window\n");
+      show_window((char*) fbuf);
+      update_window = 0;
+    }
+  }
 }
 
 #undef RES
